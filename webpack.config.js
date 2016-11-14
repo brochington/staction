320c7f7ca417222b4669a1da60871b104b1dc5b0
Broch Stilley <brochington@gmail.com>
2016-11-13 21:56:50 -0800

    Getting a basic build working.

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var path = require('path');
+var webpack = require("webpack");
+
+module.exports = {
+    entry: [
+        "./src/StateManager.js"
+    ],
+    output: {
+        path: __dirname + '/build/',
+        filename: 'bundle.js',
+        publicPath: '/build/',
+        library: "state-manager",
+        libraryTarget: "umd",
+        umdNamedDefine: true
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loader: 'babel',
+            include: path.join(__dirname, 'src'),
+            query: {
+                cacheDirectory: true,
+                presets: ["es2015", "stage-0"]
+            }
+        }]
+    },
+    resolve: {
+        extensions: ['', '.js']
+    },
+    devtool: 'source-map',
+    plugins: [
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.UglifyJsPlugin({minimize: true})
+    ]
+}
